3ff65f089c889f03684fa3aedbfa48e0eda767d8
Priyanshu <priyanshu20106@iiitd.ac.in>
2021-10-09 11:55:36 +0530

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,7 @@
 package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("hello Priyanshu");
-        System.out.println("hello");
 
     }
 }
