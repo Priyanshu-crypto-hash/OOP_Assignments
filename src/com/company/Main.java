be4b4ad2bfafc7ef8f104adfeb4f20e2820b68db
Priyanshu <priyanshu20106@iiitd.ac.in>
2021-10-09 15:51:54 +0530

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,39 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static Scanner scn = new Scanner(System.in);
+    int idCounter =0;
+
     public void addVaccine(){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Vaccine Name: ");
         String name = scn.next();
+        System.out.println("Number of Doses: ");
         int num = scn.nextInt();
-        int gap = scn.nextInt();
+        if (num!=1){
+            System.out.println("Gap between Doses: ");
+            int gap = scn.nextInt();
+            System.out.println("Vaccine Name: "+name+", Number of Doses: "+num+", Gap Between Doses: "+gap);
+        }
+        else{
+            System.out.println("Vaccine Name: "+name+", Number of Doses: "+num+", Gap Between Doses: "+0);
+        }
+
+
 }
+public void regHospital(){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Hospital Name: ");
+        idCounter+=1;
+        int uniqueId = 200000+idCounter;
+        String name = scn.next();
+        int pin = scn.nextInt();
+        System.out.println("Hospital Name: "+name+", Pincode: "+pin);
 
+
+
+}
     public static void main(String[] args) {
-
+        Scanner scn = new Scanner(System.in);
         System.out.println("CoWin Portal initialized....\n" +
                 "---------------------------------\n" +
                 "1. Add Vaccine\n" +
@@ -31,12 +55,11 @@ public class Main {
                 break;
             }
             else{
+
                 if (n==1){
 
                 }
-                else if (n==1){
 
-                }
                 else if (n==2){
 
                 }
