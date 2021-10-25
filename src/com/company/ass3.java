2b2e22461e2cbe8fbe57834f54538af6908ebfc3
Priyanshu-crypto-hash <83296467+Priyanshu-crypto-hash@users.noreply.github.com>
2021-10-25 19:16:05 +0530

    Update ass3.java

--- src/com/company/ass3.java
+++ src/com/company/ass3.java
@@ -1,1 +1,38 @@
+import java.util.concurrent.ThreadLocalRandom;
 
+ class Dice {
+    private int faceValue;
+    public Dice() {
+        roll();
+    }
+    // Rolls the dice
+    public void roll() {
+        int curr_faceValue = ThreadLocalRandom.current().nextInt(1, 2 + 1);
+        setFaceValue(curr_faceValue);
+    }
+    private void setFaceValue (int value) {
+            faceValue = value;
+    }
+     public int getFaceValue() {
+         return faceValue;
+     }
+}
+class Player{
+     private  String  playerName;
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+}
+
+class ass3 {
+     
+    public static void main(String[] args) {
+        
+    }
+}
