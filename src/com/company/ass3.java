6938c5093291f2df35cb81847d9345b69ce997a9
Priyanshu-crypto-hash <83296467+Priyanshu-crypto-hash@users.noreply.github.com>
2021-10-27 15:02:03 +0530

    Update ass3.java

--- src/com/company/ass3.java
+++ src/com/company/ass3.java
@@ -1,6 +1,19 @@
-import java.util.ArrayList;
+
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
+class Dice {
+    public static void format(){
+        Scanner scn = new Scanner(System.in);
+        System.out.println("Hit enter to roll the dice");
+        String space = scn.nextLine();
+
+    }
+    public static int rollDice() {
+
+        return ThreadLocalRandom.current().nextInt(1, 2 + 1);
+    }
+
+}
 class Player{
 
      private final String playerName;
@@ -12,14 +25,12 @@ class Player{
     protected void setTotalPoints(int totalPoints) {
         this.totalPoints = totalPoints;
     }
-
     Player(String name){
          this.playerName=name;
          }
     public int getPlayerPosition() {
         return playerPosition;
     }
-
     protected void setPlayerPosition(int playerPosition) {
         this.playerPosition = playerPosition;
     }
@@ -28,17 +39,17 @@ class Player{
     }
 }
 class Floor{
-    private Player p1;
+    private final Player p1;
     Floor(Player play1){
         this.p1=play1;
     }
-
     protected Player getP1() {
         return p1;
     }
     public void levelMessage(){
+        System.out.println("Player position Floor-"+this.getP1().getPlayerPosition());
         System.out.println(this.getP1().getPlayerName()+" has reached an Empty Floor");
-        System.out.println("Total points"+this.getP1().getTotalPoints());
+        System.out.println("Total points "+this.getP1().getTotalPoints());
     }
     public void changePosition(int diceValue){
         this.getP1().setPlayerPosition(this.getP1().getPlayerPosition()+diceValue);
@@ -56,7 +67,7 @@ class SnakeFloor extends Floor{
     public void levelMessage(){
         System.out.println("Player position Floor-"+this.getP1().getPlayerPosition());
         System.out.println(this.getP1().getPlayerName()+" has reached a normal Snake Floor");
-        System.out.println("Total points"+this.getP1().getTotalPoints());
+        System.out.println("Total points "+this.getP1().getTotalPoints());
     }
     @Override
     public void changePosition(int diceValue){
@@ -79,7 +90,7 @@ class KingCobraFloor extends SnakeFloor{
     public void levelMessage(){
         System.out.println("Player position Floor-"+this.getP1().getPlayerPosition());
         System.out.println(this.getP1().getPlayerName()+" has reached King Cobra");
-        System.out.println("Total points"+this.getP1().getTotalPoints());
+        System.out.println("Total points "+this.getP1().getTotalPoints());
     }
     @Override
     public void changePosition(int diceValue){
@@ -98,7 +109,7 @@ class LadderFloor extends Floor{
     public void levelMessage(){
         System.out.println("Player position Floor-"+this.getP1().getPlayerPosition());
         System.out.println(this.getP1().getPlayerName()+" has reached a ladder floor");
-        System.out.println("Total points"+this.getP1().getTotalPoints());
+        System.out.println("Total points "+this.getP1().getTotalPoints());
     }
     @Override
     public void changePosition(int diceValue){
@@ -117,7 +128,7 @@ class ElevatorLadder extends  LadderFloor{
     public void levelMessage(){
         System.out.println("Player position Floor-"+this.getP1().getPlayerPosition());
         System.out.println(this.getP1().getPlayerName()+" has reached elevator floor");
-        System.out.println("Total points"+this.getP1().getTotalPoints());
+        System.out.println("Total points "+this.getP1().getTotalPoints());
     }
     @Override
     public void changePosition(int diceValue){
@@ -129,89 +140,100 @@ class ElevatorLadder extends  LadderFloor{
     }
 }
 class ass3 {
-    public static  int rollDice() {
-        return ThreadLocalRandom.current().nextInt(1, 2 + 1);
-    }
-    public static void format(){
-        System.out.println("Hit enter to roll the dice");
-        String space = scn.nextLine();
 
-    }
-     public static Scanner scn = new Scanner(System.in);
-     ArrayList<Player> playerRecord = new ArrayList<>();
+
+    public static Scanner scn = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Enter the player name and hit enter");
-        String name = scn.next();
-        scn.nextLine();
+        String name = scn.nextLine();
+
         Player player1 = new Player(name);
         System.out.println("The game setup is ready");
         Floor f = new Floor(player1);
+
+
         while(true){
-            format();
-            int diceValue = rollDice();
+            Dice.format();
+            int diceValue = Dice.rollDice();
             System.out.println("Dice gave "+diceValue);
-            if(diceValue==1){
+            if (diceValue==1){
                 while(true){
-                    int _rollDice = rollDice();
-                    if (player1.getPlayerPosition()==1||player1.getPlayerPosition()==3||player1.getPlayerPosition()==4||player1.getPlayerPosition()==6||player1.getPlayerPosition()==7||player1.getPlayerPosition()==9||player1.getPlayerPosition()==10||player1.getPlayerPosition()==12){
-                        f.changePosition(_rollDice);
+                    if (f.getP1().getPlayerPosition()==0){
                         f.changePoints();
                         f.levelMessage();
-                        format();
-
+                        Dice.format();
                     }
-                    else if (player1.getPlayerPosition()==2){
-                        ElevatorLadder e = new ElevatorLadder(player1);
-                        e.changePosition(diceValue);
-                        e.changePoints();
-                        e.levelMessage();
-                        format();
-                    }
-                    else if (player1.getPlayerPosition()==5){
-                        SnakeFloor s = new SnakeFloor(player1);
-                        s.changePosition(diceValue);
-                        s.changePoints();
-                        s.levelMessage();
-                        format();
+                        int _rollDice = Dice.rollDice();
+                        System.out.println("Dice gave "+_rollDice);
 
-                    }
-                    else if (player1.getPlayerPosition()==8){
-                        LadderFloor l = new LadderFloor(player1);
-                        l.changePosition(diceValue);
-                        l.changePoints();
-                        l.levelMessage();
-                        format();
+                        if((f.getP1().getPlayerPosition()+_rollDice!=2 && f.getP1().getPlayerPosition()+_rollDice!=5 && f.getP1().getPlayerPosition()+_rollDice!=8 && f.getP1().getPlayerPosition()+_rollDice!=11 &&f.getP1().getPlayerPosition()+_rollDice!=13) && (f.getP1().getPlayerPosition()+_rollDice<13) ){
+                            f.changePosition(_rollDice);
+                            f.changePoints();
+                            f.levelMessage();
+                            Dice.format();
+                        }
+                        else if (f.getP1().getPlayerPosition()+_rollDice==2){
+                            ElevatorLadder e = new ElevatorLadder(player1);
+                            f.changePosition(_rollDice);
+                            e.changePoints();
+                            e.levelMessage();
+                            e.changePosition(_rollDice);
+                            f.changePoints();
+                            f.levelMessage();
+                            Dice.format();
+                        }
+                        else if (f.getP1().getPlayerPosition()+_rollDice==5){
+                            SnakeFloor s = new SnakeFloor(player1);
+                            f.changePosition(_rollDice);
+                            s.changePoints();
+                            s.levelMessage();
+                            s.changePosition(_rollDice);
+                            f.changePoints();
+                            f.levelMessage();
+                            Dice.format();
+                        }
+                        else if (f.getP1().getPlayerPosition()+_rollDice==8){
+                            LadderFloor l = new LadderFloor(player1);
+                            f.changePosition(_rollDice);
+                            l.changePoints();
+                            l.levelMessage();
+                            l.changePosition(_rollDice);
+                            f.changePoints();
+                            f.levelMessage();
+                            Dice.format();
+                        }
+                        else if (f.getP1().getPlayerPosition()+_rollDice==11){
+                            KingCobraFloor k = new KingCobraFloor(player1);
+                            f.changePosition(_rollDice);
+                            k.changePoints();
+                            k.levelMessage();
+                            k.changePosition(_rollDice);
+                            f.changePoints();
+                            f.levelMessage();
+                            Dice.format();
+                        }
 
-                    }
-                    else if (player1.getPlayerPosition()==11){
-                        KingCobraFloor k = new KingCobraFloor(player1);
-                        k.changePosition(diceValue);
-                        k.changePoints();
-                        k.levelMessage();
-                        format();
-
-                    }
-
-                    else if(player1.getPlayerPosition()==13){
-                        f.changePosition(_rollDice);
-                        f.changePoints();
-                        f.levelMessage();
-                        format();
-                        break;
-                    }
-
-
-
-
-                }
-
+                        else if(f.getP1().getPlayerPosition()+_rollDice==13){
+                            f.changePosition(_rollDice);
+                            f.changePoints();
+                            f.levelMessage();
+                            System.out.println("Game Over");
+                            System.out.println(f.getP1().getPlayerName()+" accumulated "+f.getP1().getTotalPoints()+" points");
+                            System.out.println("---------------------------------------------------------------");
+                            break;
+                        }
+                        else{
+                            System.out.println("Player Cannot Move");
+                            Dice.format();
+                        }
+                 }
+                break;
             }
-            else{
+            else {
                 System.out.println("Game cannot start until you get 1");
-
             }
         }
 
-        
+
     }
 }
